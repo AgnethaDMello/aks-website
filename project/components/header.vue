a7df4172ff2de1efe50812f35e0d1ed62128089d
achan2005 <110688731+ashleychching@users.noreply.github.com>
2023-07-18 15:53:58 -0400

    footer and nav shows up, issues with page showing

---
+++ project/components/header.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <h1 class="text-center">My Nuxt.js App</h1>
+      <nav class="navbar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: #333;
+  color: #fff;
+  padding: 20px;
+}
+
+.navbar {
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+}
+
+.nav {
+  list-style: none;
+  display: flex;
+}
+
+.nav-item:not(:last-child) {
+  margin-right: 20px;
+}
+
+.nav-link {
+  color: #fff;
+  text-decoration: none;
+}
+</style>
---
+++ project/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>this is the home page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
