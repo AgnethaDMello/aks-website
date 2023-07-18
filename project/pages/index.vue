4b11d63ceb83d70e5c49a7d8ec13ff58f1e9597b
AgnethaDMello <agnethad@nycstudents.net>
2023-07-18 16:10:36 -0400

    tailwind

---
+++ project/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1 class="text-3xl">Anjani K. Sinha M.D</h1>
+        <div>
+            <button>Learn More</button>
+            <button>Find a Location</button>
+        </div>
+        <h2>Ease your pain and your worries!</h2>
+    </div>
+</template>
