440a91ee903f3ab41817fad5201d48c2f3f17b27
AgnethaDMello <agnethad@nycstudents.net>
2023-07-18 18:08:13 -0400

    working on bg!

--- project/pages/index.vue
+++ project/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="font-merriweather bg-[url(aks.png)]">
-        <h1 class="text-3xl">Anjani K. Sinha M.D</h1>
+    <div class="font-merriweather bg-[url('/public/city-background-2.jpeg')]/50 bg-cover">
+        <img src="city-background-2.jpeg" alt="city" class="bg-auto opacity-[.55] z-0">
+        <h1 class="text-3xl z-10">Anjani K. Sinha M.D</h1>
         <div>
             <button>Learn More</button>
             <button>Find a Location</button>
@@ -9,3 +10,11 @@
         <img src="placeholder-doc-img.png" alt="Doctor Anjani K. Sinha">
     </div>
 </template>
+
+
+<!-- position: absolute;
+width: 671px;
+height: 343px;
+
+background: url(image.png);
+opacity: 0.55; -->
