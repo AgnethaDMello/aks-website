bd1e5930a60c170bdca4cbf6ab41847a68ea493b
achan2005 <110688731+ashleychching@users.noreply.github.com>
2023-07-29 20:48:41 -0400

    team page on hover works

--- project/pages/team.vue
+++ project/pages/team.vue
@@ -1,23 +1,34 @@
 <template>
   <div class="bg-yellow">
-    <header class="bg-medium-gray text-white p-4">
-      <h1 class="text-3xl text-center">Our Team</h1>
+    <header class="text-black p-4">
+      <h1 class="text-4xl text-left ml-28 mt-10">Our Team</h1>
     </header>
 
-    <main class="container mx-auto py-8">
+    <main class="container mx-auto py-8 pt-4">
       <div class="grid-cols-1 md:grid-cols-2 gap-8">
         <div
           v-for="doctor in doctors"
           :key="doctor.id"
-          class="flex flex-row items-center"
+          class="relative group flex flex-row items-center m-6"
         >
           <img
             :src="doctor.image"
             :alt="doctor.name"
-            class="w-80 h-80 object-cover m-5"
+            class="w-80 h-80 object-cover transition-opacity duration-300 group-hover:opacity-60"
           />
-
-          <div class="text-center md:text-left mt-4">
+          <div
+            class="absolute inset-0 bg-black opacity-0 w-80 group-hover:opacity-60 transition-opacity"
+          >
+            <div
+              class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-90 transition-opacity"
+            >
+              <div class="text-white text-center p-4">
+                <h2 class="text-xl font-bold">Contact:</h2>
+                <h2 class="text-xl">{{ doctor.phone }}</h2>
+              </div>
+            </div>
+          </div>
+          <div class="text-center md:text-left mt-4 mx-5">
             <h2 class="text-xl font-bold">{{ doctor.name }}</h2>
             <p>{{ doctor.description }}</p>
           </div>
@@ -34,21 +45,21 @@ const doctors = ref([
     id: 1,
     name: "Dr. John Adam Smith",
     image: "doctor1.jpg",
-    phone: "323-355-2245",
+    phone: "123-355-2245",
     description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sii dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
   },
   {
     id: 2,
     name: "Dr. John Adam Smith",
     image: "doctor2.jpg",
-    phone: "323-355-2245",
+    phone: "223-355-2245",
     description:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
   },
   {
     id: 3,
-    name: "Doctor 1 Name",
+    name: "Dr. Michelle Phanny",
     image: "doctor1.jpg",
     phone: "323-355-2245",
     description:
@@ -56,27 +67,27 @@ const doctors = ref([
   },
   {
     id: 4,
-    name: "Doctor 1 Name",
+    name: "Dr. Johhny Belenky",
     image: "doctor1.jpg",
-    phone: "323-355-2245",
+    phone: "423-355-2245",
     description:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
   },
   {
     id: 5,
-    name: "Doctor 1 Name",
+    name: "Dr. Lydia Smith",
     image: "doctor2.jpg",
-    phone: "323-355-2245",
+    phone: "523-355-2245",
     description:
-      "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?",
+      "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasio hooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?",
   },
   {
     id: 6,
-    name: "Doctor 1 Name",
+    name: "Dr. Janna Cena",
     image: "doctor1.jpg",
-    phone: "323-355-2245",
+    phone: "623-355-2245",
     description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitatio consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
   },
 ]);
 </script>
