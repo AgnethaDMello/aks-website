f74ba5d501269615f832c18027336dff15728510
achan2005 <110688731+ashleychching@users.noreply.github.com>
2023-08-09 17:04:32 -0400

    done with knee

--- project/pages/knee.vue
+++ project/pages/knee.vue
@@ -11,28 +11,38 @@
         Knee Services
       </h1>
     </div>
-    <div>
-      <img src="red knee.png" alt="pain on knee" />
-      <h2>What Is The Anterior Cruciate Ligament (ACL)?</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Gravida neque
-        convallis a cras. Arcu cursus euismod quis viverra. Egestas egestas
-        fringilla phasellus faucibus scelerisque eleifend. Sed blandit libero
-        volutpat sed cras ornare arcu dui vivamus. Vulputate eu scelerisque
-        felis imperdiet proin fermentum leo vel. Felis bibendum ut tristique et
-        egestas. Ligula ullamcorper malesuada proin libero nunc consequat
-        interdum. Egestas erat imperdiet sed euismod nisi porta lorem mollis
-        aliquam. Egestas fringilla phasellus faucibus scelerisque eleifend donec
-        pretium. Lacus suspendisse faucibus interdum posuere lorem ipsum. Massa
-        enim nec dui nunc mattis enim. Felis donec et odio pellentesque. Aenean
-        et tortor at risus viverra.
-      </p>
+    <div class="relative flex items-center justify-between mx-[5%] mt-[3rem]">
+      <div class="w-2/3 pr-8">
+        <h2 class="text-yellow-txt text-2xl mb-[5%]">
+          What Is The Anterior Cruciate Ligament (ACL)?
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida
+          neque convallis a cras. Arcu cursus euismod quis viverra. Egestas
+          egestas fringilla phasellus faucibus scelerisque eleifend. Sed blandit
+          libero volutpat sed cras ornare arcu dui vivamus. Vulputate eu
+          scelerisque felis imperdiet proin fermentum leo vel. Felis bibendum ut
+          tristique et egestas. Ligula ullamcorper malesuada proin libero nunc
+          consequat interdum. Egestas erat imperdiet sed euismod nisi porta
+          lorem mollis aliquam. Egestas fringilla phasellus faucibus scelerisque
+          eleifend donec pretium. Lacus suspendisse faucibus interdum posuere
+          lorem ipsum. Massa enim nec dui nunc mattis enim. Felis donec et odio
+          pellentesque. Aenean et tortor at risus viverra.
+        </p>
+      </div>
+      <div class="w-1/3">
+        <img
+          src="red knee.png"
+          alt="pain on knee"
+          class="w-full max-w-md mx-auto"
+        />
+      </div>
     </div>
 
     <hr class="my-12 h-0.5 border-t-0 bg-black opacity-100 dark:opacity-100" />
-    <div>
-      <h2>How we can help</h2>
+    <div class="pb-[3rem] mx-[5%]">
+      <h2 class="text-yellow-txt text-2xl mb-[2rem]">How we can help</h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Gravida neque
