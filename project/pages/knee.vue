ccd4b9d44c8ddbedf0f8f6e5f3b8e7ae9183c4d1
achan2005 <110688731+ashleychching@users.noreply.github.com>
2023-08-09 15:40:56 -0400

    knee

--- project/pages/knee.vue
+++ project/pages/knee.vue
@@ -1,5 +1,54 @@
 <template>
-  <div></div>
+  <div class="bg-yellow">
+    <div class="relative">
+      <img
+        src="running.jpeg"
+        alt="Running header image"
+        class="h-[30vh] w-[100vw] object-cover"
+        style="object-position: center bottom"
+      />
+      <h1 class="absolute top-1/4 text-white text-4xl ml-[5%] mt-10">
+        Knee Services
+      </h1>
+    </div>
+    <div>
+      <img src="red knee.png" alt="pain on knee" />
+      <h2>What Is The Anterior Cruciate Ligament (ACL)?</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Gravida neque
+        convallis a cras. Arcu cursus euismod quis viverra. Egestas egestas
+        fringilla phasellus faucibus scelerisque eleifend. Sed blandit libero
+        volutpat sed cras ornare arcu dui vivamus. Vulputate eu scelerisque
+        felis imperdiet proin fermentum leo vel. Felis bibendum ut tristique et
+        egestas. Ligula ullamcorper malesuada proin libero nunc consequat
+        interdum. Egestas erat imperdiet sed euismod nisi porta lorem mollis
+        aliquam. Egestas fringilla phasellus faucibus scelerisque eleifend donec
+        pretium. Lacus suspendisse faucibus interdum posuere lorem ipsum. Massa
+        enim nec dui nunc mattis enim. Felis donec et odio pellentesque. Aenean
+        et tortor at risus viverra.
+      </p>
+    </div>
+
+    <hr class="my-12 h-0.5 border-t-0 bg-black opacity-100 dark:opacity-100" />
+    <div>
+      <h2>How we can help</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Gravida neque
+        convallis a cras. Arcu cursus euismod quis viverra. Egestas egestas
+        fringilla phasellus faucibus scelerisque eleifend. Sed blandit libero
+        volutpat sed cras ornare arcu dui vivamus. Vulputate eu scelerisque
+        felis imperdiet proin fermentum leo vel. Felis bibendum ut tristique et
+        egestas. Ligula ullamcorper malesuada proin libero nunc consequat
+        interdum. Egestas erat imperdiet sed euismod nisi porta lorem mollis
+        aliquam. Egestas fringilla phasellus faucibus scelerisque eleifend donec
+        pretium. Lacus suspendisse faucibus interdum posuere lorem ipsum. Massa
+        enim nec dui nunc mattis enim. Felis donec et odio pellentesque. Aenean
+        et tortor at risus viverra.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
