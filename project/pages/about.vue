2f2a1d05d3218c63e034bcbd2d0739c118d76962
achan2005 <110688731+ashleychching@users.noreply.github.com>
2023-08-16 14:40:18 -0400

    Revert "Merge branch 'annie' into develop"
    This reverts commit 9d5055b2b24a319b752332d9696454a74960a8e8, reversing
    changes made to 55be56e4376e91c08c268330a435df8b8b1ef0e2.

--- project/pages/about.vue
+++ project/pages/about.vue
@@ -52,7 +52,7 @@
       </div>
     </div>
     <div class="bg-light-gray h-[50rem]">
-      <div class="w-[55%] pt-[10rem] float-left">
+      <div class="w-[40%] pt-[10rem] float-left">
         <h2 class="w-[55%] ml-[10%] text-6xl text-brown">
           We care for you as a person
         </h2>
@@ -64,85 +64,15 @@
           Aenean et tortor at risus viverra.
         </p>
       </div>
-      <svg
-        class="absolute ml-[63%] mt-4"
-        height="170px"
-        width="170px"
-        version="1.1"
-        id="Capa_1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        viewBox="0 0 46.195 46.195"
-        xml:space="preserve"
-        fill="#000000"
-        transform="matrix(-1, 0, 0, -1, 0, 0)"
-      >
-        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-        <g
-          id="SVGRepo_tracerCarrier"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></g>
-        <g id="SVGRepo_iconCarrier">
-          <g>
-            <path
-              style="fill: #010002"
-              d="M35.765,8.264c-5.898,0-10.555,4.782-10.555,10.68s4.844,10.68,10.742,10.68 c0.059,0,0.148-0.008,0.207-0.009c-2.332,1.857-5.261,2.976-8.467,2.976c-1.475,0-2.662,1.196-2.662,2.67s0.949,2.67,2.424,2.67 c10.469-0.001,18.741-8.518,18.741-18.987c0-0.002,0-0.004,0-0.007C46.195,13.042,41.661,8.264,35.765,8.264z"
-            ></path>
-            <path
-              style="fill: #010002"
-              d="M10.75,8.264c-5.898,0-10.563,4.782-10.563,10.68s4.84,10.68,10.739,10.68 c0.059,0,0.146-0.008,0.205-0.009c-2.332,1.857-5.262,2.976-8.468,2.976C1.188,32.591,0,33.787,0,35.261s0.964,2.67,2.439,2.67 c10.469-0.001,18.756-8.518,18.756-18.987c0-0.002,0-0.004,0-0.007C21.195,13.042,16.646,8.264,10.75,8.264z"
-            ></path>
-          </g>
-        </g>
-      </svg>
       <div
-        class="w-[35%] h-[70%] mt-[5%] mr-20 float-right bg-gradient-to-br from-yellow to-light-yellow flex flex-col justify-center items-center"
-      >
-        <slideshow class="m-12" />
-      </div>
-      <svg
-        class="absolute ml-[84%] mt-[29%]"
-        height="170px"
-        width="170px"
-        version="1.1"
-        id="Capa_1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        viewBox="0 0 46.195 46.195"
-        xml:space="preserve"
-        fill="#000000"
-        transform="matrix(1, 0, 0, 1, 0, 0)"
-      >
-        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-        <g
-          id="SVGRepo_tracerCarrier"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></g>
-        <g id="SVGRepo_iconCarrier">
-          <g>
-            <path
-              style="fill: #010002"
-              d="M35.765,8.264c-5.898,0-10.555,4.782-10.555,10.68s4.844,10.68,10.742,10.68 c0.059,0,0.148-0.008,0.207-0.009c-2.332,1.857-5.261,2.976-8.467,2.976c-1.475,0-2.662,1.196-2.662,2.67s0.949,2.67,2.424,2.67 c10.469-0.001,18.741-8.518,18.741-18.987c0-0.002,0-0.004,0-0.007C46.195,13.042,41.661,8.264,35.765,8.264z"
-            ></path>
-            <path
-              style="fill: #010002"
-              d="M10.75,8.264c-5.898,0-10.563,4.782-10.563,10.68s4.84,10.68,10.739,10.68 c0.059,0,0.146-0.008,0.205-0.009c-2.332,1.857-5.262,2.976-8.468,2.976C1.188,32.591,0,33.787,0,35.261s0.964,2.67,2.439,2.67 c10.469-0.001,18.756-8.518,18.756-18.987c0-0.002,0-0.004,0-0.007C21.195,13.042,16.646,8.264,10.75,8.264z"
-            ></path>
-          </g>
-        </g>
-      </svg>
+        class="w-[45%] h-[70%] mt-[5%] mr-20 float-right bg-gradient-to-br from-yellow to-light-yellow"
+      ></div>
     </div>
   </div>
 </template>
 
 <script>
-import slideshow from "@/components/slideshow";
-
-export default {
-  components: {
-    slideshow,
-  },
-};
+export default {};
 </script>
+
+<style lang="scss"></style>
--- project/pages/consultations.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        <h1>Consultations</h1>
-    </div>
-</template>
-
-<script>
-</script>
