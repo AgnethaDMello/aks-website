26b09e617a58b0e4fbeb3859dbc56d1cb996ed04
achan2005 <110688731+ashleychching@users.noreply.github.com>
2023-08-16 14:08:56 -0400

    conditions

--- project/pages/shoulder.vue
+++ project/pages/shoulder.vue
@@ -57,11 +57,128 @@
         et tortor at risus viverra.
       </p>
     </div>
+    <div class="max-w-full mx-auto bg-gradient-to-b from-yellow to-white mb-16">
+      <h1 class="text-2xl font-semibold pb-[3rem] pt-4 mx-[5%]">
+        Conditions We Treat
+      </h1>
+      <div
+        v-for="(condition, index) in conditions"
+        :key="index"
+        class="bg-white p-4 rounded shadow mb-4 max-w-[80%] mx-auto"
+      >
+        <div class="flex justify-between items-center">
+          <h2 class="text-lg font-semibold">{{ condition.title }}</h2>
+          <button
+            class="text-blue-500 focus:outline-none"
+            @click="toggleCondition(index)"
+          >
+            {{ condition.show ? "-" : "+" }}
+          </button>
+        </div>
+        <ul v-if="condition.show" class="list-disc list-inside ml-4 pt-4">
+          <div class="flex flex-wrap">
+            <div class="w-full md:w-2/3 whitespace-pre-line pb-4">
+              <p>{{ condition.explanation }}</p>
+            </div>
+            <div class="w-full md:w-1/3 lg:w-1/4 mx-auto 2xl:w-1/5">
+              <img
+                :src="`/public/${condition.img}`"
+                :alt="condition.title"
+                class="w-[90%] mx-auto h-[100%] object-cover"
+              />
+            </div>
+          </div>
+          <h3 class="py-4 font-medium">Treatment options</h3>
+          <li
+            v-for="(treatment, treatmentIndex) in condition.treatments"
+            :key="treatmentIndex"
+          >
+            {{ treatment }}
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      conditions: [
+        {
+          title: "Condition 1",
+          explanation:
+            "É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como o modelo de texto usado por omissão, e uma pesquisa por irá encontrar muitos websites ainda na sua infância. Várias versões têm evoluído ao longo dos anos, por vezes por acidente, por vezes propositadamente (como no caso do humor)",
+          treatments: [
+            "Treatment 1 for Condition 1",
+            "Treatment 2 for Condition 1",
+          ],
+          img: `red knee.png`,
+          show: false,
+        },
+        {
+          title: "Condition 1",
+          explanation:
+            "É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como o modelo de texto usado por omissão, e uma pesquisa por irá encontrar muitos websites ainda na sua infância. Várias versões têm evoluído ao longo dos anos, por vezes por acidente, por vezes propositadamente (como no caso do humor)",
+          treatments: [
+            "Treatment 1 for Condition 1",
+            "Treatment 2 for Condition 1",
+          ],
+          img: `red knee.png`,
+          show: false,
+        },
+        {
+          title: "Condition 1",
+          explanation:
+            "É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como o modelo de texto usado por omissão, e uma pesquisa por irá encontrar muitos websites ainda na sua infância. Várias versões têm evoluído ao longo dos anos, por vezes por acidente, por vezes propositadamente (como no caso do humor)",
+          treatments: [
+            "Treatment 1 for Condition 1",
+            "Treatment 2 for Condition 1",
+          ],
+          img: `red knee.png`,
+          show: false,
+        },
+        {
+          title: "Condition 1",
+          explanation:
+            "É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como o modelo de texto usado por omissão, e uma pesquisa por irá encontrar muitos websites ainda na sua infância. Várias versões têm evoluído ao longo dos anos, por vezes por acidente, por vezes propositadamente (como no caso do humor)",
+          treatments: [
+            "Treatment 1 for Condition 1",
+            "Treatment 2 for Condition 1",
+          ],
+          img: `red knee.png`,
+          show: false,
+        },
+        {
+          title: "Condition 1",
+          explanation:
+            "É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como o modelo de texto usado por omissão, e uma pesquisa por irá encontrar muitos websites ainda na sua infância. Várias versões têm evoluído ao longo dos anos, por vezes por acidente, por vezes propositadamente (como no caso do humor)",
+          treatments: [
+            "Treatment 1 for Condition 1",
+            "Treatment 2 for Condition 1",
+          ],
+          img: `red knee.png`,
+          show: false,
+        },
+        {
+          title: "Condition 1",
+          explanation:
+            "É um facto estabelecido de que um leitor é distraído pelo conteúdo legível de uma página quando analisa a sua mancha gráfica. Logo, o uso de Lorem Ipsum leva a uma distribuição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como ição mais ou menos normal de letras, ao contrário do uso de , tornando-o texto legível. Muitas ferramentas de publicação electrónica e editores de páginas web usam actualmente o Lorem Ipsum como o modelo de texto usado por omissão, e uma pesquisa por irá encontrar muitos websites ainda na sua infância. Várias versões têm evoluído ao longo dos anos, por vezes por acidente, por vezes propositadamente (como no caso do humor)",
+          treatments: [
+            "Treatment 1 for Condition 1",
+            "Treatment 2 for Condition 1",
+          ],
+          img: `red knee.png`,
+          show: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleCondition(index) {
+      this.conditions[index].show = !this.conditions[index].show;
+    },
+  },
+};
 </script>
-
-<style lang="scss"></style>
