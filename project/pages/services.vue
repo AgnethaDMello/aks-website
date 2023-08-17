1769e23b387071cfe7fe4168e7a70925efa049e7
achan2005 <110688731+ashleychching@users.noreply.github.com>
2023-08-17 14:31:46 -0400

    update footer

--- project/pages/services.vue
+++ project/pages/services.vue
@@ -7,12 +7,12 @@
     <h1 class="lg:text-7xl text-3xl text-dark-gray text-center pt-5 mb-10 px-4">
       Our Services For You
     </h1>
-    <div class="md:flex justify-around lg:mt-20 mt-10 mx-10">
+    <div class="md:flex justify-around lg:mt-20 mt-10 mx-20">
       <div
         class="bg-light-yellow md:w-[30%] md:h-[32rem] lg:h-[36rem] flex flex-col text-center items-center mb-8"
       >
         <h3
-          class="lg:text-5xl text-xl w-[100%] bg-dark-gray mb-7 p-7 text-white"
+          class="lg:text-4xl text-lg w-[100%] bg-dark-gray mb-7 p-7 text-white"
         >
           Knee
         </h3>
@@ -31,7 +31,7 @@
         class="bg-light-yellow md:w-[30%] md:h-[32rem] lg:h-[36rem] flex flex-col text-center items-center mb-8"
       >
         <h3
-          class="lg:text-5xl text-xl w-[100%] bg-dark-gray mb-7 p-7 text-white"
+          class="lg:text-4xl text-lg w-[100%] bg-dark-gray mb-7 p-7 text-white"
         >
           Shoulder
         </h3>
@@ -50,7 +50,7 @@
         class="bg-light-yellow md:w-[30%] md:h-[32rem] lg:h-[36rem] flex flex-col text-center items-center"
       >
         <h3
-          class="lg:text-5xl text-xl w-[100%] bg-dark-gray mb-7 p-7 text-white"
+          class="lg:text-4xl text-lg w-[100%] bg-dark-gray mb-7 p-7 text-white"
         >
           Consultations
         </h3>
